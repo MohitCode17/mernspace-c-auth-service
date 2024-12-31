2d4328c9bf7b8427f0f50ced70120930af7a3afd
MohitCode17 <mohit.codes17@gmail.com>
2024-12-31 15:31:30 +0530

    add test for firstName missing

--- src/validators/register-validator.ts
+++ src/validators/register-validator.ts
@@ -6,6 +6,11 @@ export default checkSchema({
     notEmpty: true,
     trim: true,
   },
+  firstName: {
+    errorMessage: "FirstName is required!",
+    notEmpty: true,
+    trim: true,
+  },
 });
 
 // export default [body("email").notEmpty().withMessage("Email is required!")];
--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -261,6 +261,25 @@ describe("POST /auth/register", () => {
       const users = await userRepository.find();
       expect(users).toHaveLength(0);
     });
+
+    it("should return 400 status code if firstName field is missing", async () => {
+      // Arrange
+      const userData = {
+        firstName: "",
+        lastName: "Gupta",
+        email: "mohit@mern.space",
+        password: "password",
+      };
+
+      // Act
+      const response = await request(app).post("/auth/register").send(userData);
+
+      // Assert
+      expect(response.statusCode).toBe(400);
+      const userRepository = connection.getRepository(User);
+      const users = await userRepository.find();
+      expect(users).toHaveLength(0);
+    });
   });
 
   describe("Fields are not in proper format", () => {
