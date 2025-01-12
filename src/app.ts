fce981201f1bcaf9006ebb77a3992cbcf49541e2
MohitCode17 <mohit.codes17@gmail.com>
2025-01-12 22:23:19 +0530

    add cors

--- src/app.ts
+++ src/app.ts
@@ -7,8 +7,15 @@ import authRouter from "./routes/auth";
 import tenantRouter from "./routes/tenant";
 import userRouter from "./routes/user";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  }),
+);
 
 // Middlewares
 app.use(express.static("public"));
